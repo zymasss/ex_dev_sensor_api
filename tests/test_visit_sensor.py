7b8db168c7cf0dbca9438f14a9c944cbc5dd5b7b
pauzon <pauzon@gmail.com>
2023-10-31 17:15:45 +0100

    simplify tests

--- tests/test_visit_sensor.py
+++ tests/test_visit_sensor.py
@@ -4,46 +4,16 @@ from datetime import date
 
 
 class TestVisitSensor(unittest.TestCase):
-    def test_monday_open(self):
-        visit_sensor = VisitSensor(1200, 300)
-        visit_count = visit_sensor.simulate_visit_count(date(2023, 9, 11))
-
-        self.assertFalse(visit_count == -1)
-
-    def test_tuesday_open(self):
-        visit_sensor = VisitSensor(1200, 300)
-        visit_count = visit_sensor.simulate_visit_count(date(2023, 9, 12))
-
-        self.assertFalse(visit_count == -1)
-
-    def test_wednesday_open(self):
-        visit_sensor = VisitSensor(1200, 300)
-        visit_count = visit_sensor.simulate_visit_count(date(2023, 9, 13))
-
-        self.assertFalse(visit_count == -1)
-
-    def test_thursday_open(self):
-        visit_sensor = VisitSensor(1200, 300)
-        visit_count = visit_sensor.simulate_visit_count(date(2023, 9, 14))
-
-        self.assertFalse(visit_count == -1)
-
-    def test_friday_open(self):
-        visit_sensor = VisitSensor(1200, 300)
-        visit_count = visit_sensor.simulate_visit_count(date(2023, 9, 15))
-
-        self.assertFalse(visit_count == -1)
-
-    def test_saturday_open(self):
-        visit_sensor = VisitSensor(1200, 300)
-        visit_count = visit_sensor.simulate_visit_count(date(2023, 9, 16))
-
-        self.assertFalse(visit_count == -1)
+    def test_weekdays_open(self):
+        for test_day in range(11, 17):
+            with self.subTest(i=test_day):
+                visit_sensor = VisitSensor(1200, 300)
+                visit_count = visit_sensor.simulate_visit_count(date(2023, 9, test_day))
+                self.assertFalse(visit_count == -1)
 
     def test_sunday_closed(self):
         visit_sensor = VisitSensor(1200, 300)
         visit_count = visit_sensor.simulate_visit_count(date(2023, 9, 17))
-
         self.assertEqual(visit_count, -1)
 
     def test_with_break(self):
